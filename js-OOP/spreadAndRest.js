c5d79fb14525e2e891e2bc35adf86a22c0e3f2b5
RostyslavKov <rostyslav.kovalenko@gmail.com>
2023-10-16 10:53:50 +0300

    Added Homework 7

---
+++ js-OOP/spreadAndRest.js
@@ -0,0 +1,18 @@
+//Task №1
+console.log("Task №1");
+
+const array1 = [1, 2, 3, 4, 5];
+const array2 = [6, 7, 8, 9, 10];
+const combinedArray = [...array1, ...array2]; 
+
+console.log(combinedArray);
+
+//Task №2
+console.log("Task №2");
+
+function multiply(multiplier, ...arguments) {
+    return arguments.map((value) => multiplier * value);
+}
+  
+const result = multiply(4, 9, 83, 3);
+console.log(result); 
